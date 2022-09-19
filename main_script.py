c21f147e52d338d9f0f535916d3b31a4249136bb
Jack JRZ <jackjereza.08@gmail.com>
2022-09-19 13:12:07 +0800

    Modified main_script

--- main_script.py
+++ main_script.py
@@ -1,17 +1,26 @@
 from getpass import getpass
 import mysql.connector as connector
+from mysql.connector import Error
 
-conn = connector.connect(
-        host='localhost',
-        user= input("Username:"),
-        passwd=getpass("Password:"))
 
-cursor = conn.cursor()
-create_database = "CREATE DATABASE print_pos"
-cursor.execute(create_database)
+class Main:
+    def __init__(self):
+        try:
+            conn = connector.connect(
+                    host='localhost',
+                    user= input("Username:"),
+                    passwd=getpass("Password:"))
 
-print(conn)
-print(cursor.execute("SHOW DATABASES"))
+            cursor = conn.cursor()
+            create_database = "CREATE DATABASE db_print_pos"
+            cursor.execute(create_database)
 
-conn.close()
+            print(conn)
+            print(cursor.execute("SHOW DATABASES"))
+
+            conn.close()
+        except Error as e:
+            print(e)
+
+Main()
 
