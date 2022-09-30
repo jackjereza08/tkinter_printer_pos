422a389f3afc0b6d84d7a15274e0f1462de7574b
Jack JRZ <jackjereza.08@gmail.com>
2022-09-30 15:10:21 +0800

    Modified script

--- scripts/transaction_script.py
+++ scripts/transaction_script.py
@@ -1,5 +1,7 @@
-import mysql.connector as connector
+from scripts.db_script import MySQLConnector
 from mysql.connector import Error
+
+db = MySQLConnector()
 
 class TransactionSum:
     def sales_summary_all(self):
